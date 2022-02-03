737689f468201a0d343b1dff7c74676b1934e94d
Joaquin <89862918+JoacoDiSant@users.noreply.github.com>
2022-02-03 13:32:49 -0300

    fix - Cards terminado

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,15 +1,23 @@
-
 import React from "react";
-
-
-
+import Cards from "./Cards";
+import hoteles from "../harcodeado.json"
 
 export default function Home() {
   return (
-    <div>
-    </div>
+  <div>
+    {
+      hoteles.map((e) => {
+        return(
+          <Cards 
+            name={e.name}
+            id={e.id}
+            location={e.location}
+            img={e.img}
+            price={e.pricePerNight}
+          />
+        )
+      })
+    }
+  </div>
   );
 }
-
-
- 
