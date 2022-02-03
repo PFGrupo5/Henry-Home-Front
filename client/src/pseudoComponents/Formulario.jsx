0aa8857560a97300f390506c14b0267694a03b5b
Joaquin <89862918+JoacoDiSant@users.noreply.github.com>
2022-02-03 19:00:54 -0300

    fix - Login Form

--- client/src/pseudoComponents/Formulario.jsx
+++ client/src/pseudoComponents/Formulario.jsx
@@ -1,12 +1,63 @@
 import React from "react";
-// import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
+export default function Login({ nombre }) {
+  if (nombre === "SignIn") {
+    return (
+      <div>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your Username!" }]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your Password!" }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
 
-export default function Register({prop}) {
-  console.log(prop);
-  return (
-    <div>
-      <h1>hola?</h1>
-    </div>
-  );
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Log in
+            </Button>
+            Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>hola, soy el Register</h1>
+      </div>
+    );
+  }
 }
