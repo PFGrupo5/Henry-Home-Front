c2053d33cbb93a4111a96e09a8d8d815ce51902e
TomasGieco <tomas.gieco@hotmail.com>
2022-02-03 02:38:48 -0300

    First Landing?

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,30 +1,32 @@
+import React, { useRef, useState } from 'react';
 
-import React from "react";
 import "antd/dist/antd.css";
-import "../assets/css/Landing/Landing.css";
+import "../assets/css/Landing/Landing.css"
+import img1 from "../assets/img/h1.jfif"
+import img2 from "../assets/img/h2.jfif"
+import NavBar from './NavBar';
+import Carrousel from '../pseudoComponents/Carrousel'
+import Button from '../pseudoComponents/Button'
 
-import { Carousel, Image } from "antd";
-import img1 from "../assets/img/h1.jfif";
-import img2 from "../assets/img/h2.jfif";
+const stylesButton = {
+    backgroundColor: "#092c4cef",
+    borderRadius: 10,
+    fontSize: 25,
+    paddingBottom: 40,
+    width: "50%",
+    fontWeight: 600,
+}
 
 function LandingPage() {
     return (
-        <div className="all">
-            <Carousel autoplay autoplaySpeed={5000}>
-                <div>
-                    <Image preview={false} pauseOnHover src={img1} className="img" />
-                </div>
-                <div>
-                    <Image preview={false} pauseOnHover src={img2} className="img" />
-                </div>
-                <div>
-                    <Image preview={false} pauseOnHover src={img1} className="img" />
-                </div>
-                <div>
-                    <Image preview={false} pauseOnHover src={img2} className="img" />
-                </div>
-            </Carousel>
-        </div>
+        <div>
+            <NavBar />
+            <div className='allHome'>
+                <h1 className='textHome'>One click away from your new Journey</h1>
+                <Button href="/home" className="btn" styles={stylesButton} types="ghost" text={"HOME"} />
+            </div>
+            <Carrousel imgs={[img1, img2, img1, img2]} />
+        </div >
     );
 }
 
