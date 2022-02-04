7698cb02839856795092f4aef16067fe2024cb80
MateoBonavia <mateobonavia@gmail.com>
2022-02-04 00:58:39 -0300

    Aside

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,23 +1,24 @@
 import React from "react";
-import Cards from "./Cards";
-import hoteles from "../harcodeado.json"
+// import Cards from "./Cards";
+// import hoteles from "../harcodeado.json";
+// import Footer from "./Footer";
+import Aside from "./Aside/Aside";
 
 export default function Home() {
   return (
-  <div>
-    {
-      hoteles.map((e) => {
-        return(
-          <Cards 
+    <div>
+      <Aside>/</Aside>
+      {/* {hoteles.map((e) => {
+        return (
+          <Cards
             name={e.name}
             id={e.id}
             location={e.location}
             img={e.img}
             price={e.pricePerNight}
           />
-        )
-      })
-    }
-  </div>
+        );
+      })} */}
+    </div>
   );
 }
