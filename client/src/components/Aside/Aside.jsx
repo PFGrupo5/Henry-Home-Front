a5952797bcb21720a8a16046589fec2efdc78a57
MateoBonavia <mateobonavia@gmail.com>
2022-02-06 18:22:44 -0300

    Aside

--- client/src/components/Aside/Aside.jsx
+++ client/src/components/Aside/Aside.jsx
@@ -1,34 +1,34 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import "../../assets/css/Aside/Aside.css";
-import Calification from "./Aside complements/Calification/Calification";
-import Price from "./Aside complements/Price/Price";
-import Place from "./Aside complements/Place/Place";
-import People from "./Aside complements/People/People";
 import { MenuOutlined, CloseOutlined } from "@ant-design/icons";
+import "../../assets/css/Aside/Aside.css";
+import Calification from "./Calification";
+import People from "./People";
+import Place from "./Place";
+import Price from "./Price";
 
 export default function Aside() {
   const [sidebar, setSidebar] = useState(false);
 
-  const showSidebar = () => setSidebar(!sidebar);
-  return (
-    <div className="filters">
-      <Link to="#" className="menu-bars">
-        <MenuOutlined onClick={showSidebar} className="menu-icons" />
-      </Link>
+  const showSidebar = () => {
+    setSidebar(!sidebar);
+  };
 
-      <div className={sidebar ? "nav-menu active" : "nav-menu"}>
-        <ul className="nav-menu-items">
-          <li className="navbar-toggle">
-            <Link to="#" className="menu-bars">
-              <CloseOutlined onClick={showSidebar} className="menu-icons-x" />
-            </Link>
-          </li>
+  return (
+    <div className="containerOfContainers">
+      {sidebar === false ? (
+        <MenuOutlined onClick={showSidebar} className="buttons" />
+      ) : (
+        <CloseOutlined onClick={showSidebar} className="buttons" />
+      )}
+      <div className="generalContainer">
+        <div
+          className={sidebar ? "filtersContainer active" : "filtersContainer"}
+        >
           <Calification />
-          <Price />
-          <Place />
           <People />
-        </ul>
+          <Place />
+          <Price />
+        </div>
       </div>
     </div>
   );
---
+++ client/src/components/Aside/Calification.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Cascader } from "antd";
+import "../../assets/css/Aside/Components/Aside-components.css";
+
+export default function Calification() {
+  const options = [
+    {
+      value: "oneStar",
+      label: "⭐",
+    },
+    {
+      value: "twoStar",
+      label: "⭐⭐",
+    },
+    {
+      value: "threeStar",
+      label: "⭐⭐⭐",
+    },
+    {
+      value: "fourStar",
+      label: "⭐⭐⭐⭐",
+    },
+    {
+      value: "fiveStar",
+      label: "⭐⭐⭐⭐⭐",
+    },
+  ];
+
+  function onChange(value) {
+    console.log(value);
+  }
+
+  return (
+    <div>
+      <Cascader
+        className="filter"
+        options={options}
+        onChange={onChange}
+        placeholder="Calification..."
+      />
+    </div>
+  );
+}
