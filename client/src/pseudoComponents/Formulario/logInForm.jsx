2edb52d760f86d018a19e13656e71db064c992c5
Joaquin <joacodi2001@gmail.com>
2022-02-10 09:28:44 -0300

    Login-Register Landing con estilos Casi Completo

--- client/src/pseudoComponents/Formulario/logInForm.jsx
+++ client/src/pseudoComponents/Formulario/logInForm.jsx
@@ -1,72 +1,63 @@
-import React from 'react';
+import React from "react";
 import { Form, Input, Button, Checkbox } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import "../../assets/pseudoCss/Form/Form Login/FormLogin.css";
 
-function LogInForm({ landing }) {
-    if (!landing) {
-        return (
-            <div>
-                <Form
-                    name="normal_login"
-                    className="login-form"
-                    initialValues={{ remember: true }}
-                >
-                    <Form.Item
-                        name="username"
-                        rules={[{ required: true, message: "Please input your Username!" }]}
-                    >
-                        <Input
-                            prefix={<UserOutlined className="site-form-item-icon" />}
-                            placeholder="Username"
-                        />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[{ required: true, message: "Please input your Password!" }]}
-                    >
-                        <Input
-                            prefix={<LockOutlined className="site-form-item-icon" />}
-                            type="password"
-                            placeholder="Password"
-                        />
-                    </Form.Item>
-                    <Form.Item>
-                        <Form.Item name="remember" noStyle>
-                            <Checkbox>Remember me</Checkbox>
-                        </Form.Item>
+function LogInForm() {
+  return (
+    <div className="formLOGIN">
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+      >
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: "Please input your Username!" }]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+          <p className="login-form-forgot">Forgot password</p>
+        </Form.Item>
 
-                        <p className="login-form-forgot" >
-                            Forgot password
-                        </p>
-                    </Form.Item>
+        <Form.Item className="btns-Log-Reg">
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="Btn-login"
+          >
+            Log in
+          </Button>
+          <p className="or">Or</p>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="Btn-register"
+          >
+            Register Now
+          </Button>
 
-                    <Form.Item>
-                        <Button
-                            type="primary"
-                            htmlType="submit"
-                            className="login-form-button"
-                        >
-                            Log in
-                        </Button>
-                        Or <p>register now!</p>
-                    </Form.Item>
-                </Form>
-            </div>
-        );
-    }
-    else {
-        return (
-            <div className="container__form container--signup">
-                <form action="#" class="form" id="form1">
-                    <h2 className="form__title">Sign Up</h2>
-                    <input type="text" placeholder="User" className="input" />
-                    <input type="email" placeholder="Email" className="input" />
-                    <input type="password" placeholder="Password" className="input" />
-                    <button className="formBtn">Sign Up</button>
-                </form>
-            </div>
-        )
-    }
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
 
 export default LogInForm;
--- client/src/pseudoComponents/Formulario/registerForm.jsx
+++ client/src/pseudoComponents/Formulario/registerForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "antd/dist/antd.css";
+import "../../assets/pseudoCss/Form/Form Register/formRegister.css";
 import { Form, Input, Row, Col, Checkbox, Button } from "antd";
 import ReCAPTCHA from "react-google-recaptcha";
 
@@ -41,122 +41,124 @@ export default function RegisterForm() {
     console.log("Received values of form: ", values);
   };
   return (
-    <Form
-      {...formItemLayout}
-      form={form}
-      name="register"
-      onFinish={onFinish}
-      scrollToFirstError
-    >
-      <Form.Item
-        name="email"
-        label="E-mail"
-        rules={[
-          {
-            type: "email",
-            message: "The input is not valid E-mail!",
-          },
-          {
-            required: true,
-            message: "Please input your E-mail!",
-          },
-        ]}
+    <div className="formREGISTER">
+      <Form
+        {...formItemLayout}
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        scrollToFirstError
       >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your password!",
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
-        name="confirm"
-        label="Confirm Password"
-        dependencies={["password"]}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Please confirm your password!",
-          },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue("password") === value) {
-                return Promise.resolve();
-              }
-
-              return Promise.reject(
-                new Error("The two passwords that you entered do not match!")
-              );
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!",
             },
-          }),
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+            {
+              required: true,
+              message: "Please input your E-mail!",
+            },
+          ]}
+        >
+          <Input  placeholder="E-Mail"/>
+        </Form.Item>
 
-      <Form.Item
-        name="nickname"
-        label="Nickname"
-        tooltip="What do you want others to call you?"
-        rules={[
-          {
-            required: true,
-            message: "Please input your nickname!",
-            whitespace: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password  placeholder="Password"/>
+        </Form.Item>
 
-      <Form.Item
-        label="Captcha"
-        extra="We must make sure that your are a human."
-      >
-        <Row gutter={8}>
-          <Col span={12}>
-            <Form.Item name="captcha" noStyle>
-              <div className="recaptcha">
-                <ReCAPTCHA sitekey="6LceUGQeAAAAACkNOhVZv0OjckU1BTVA8-WE7Xf0" />
-              </div>
-            </Form.Item>
-          </Col>
-        </Row>
-      </Form.Item>
+        <Form.Item
+          name="confirm"
+          label="Password"
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
 
-      <Form.Item
-        name="agreement"
-        valuePropName="checked"
-        rules={[
-          {
-            validator: (_, value) =>
-              value
-                ? Promise.resolve()
-                : Promise.reject(new Error("Should accept agreement")),
-          },
-        ]}
-        {...tailFormItemLayout}
-      >
-        <Checkbox>
-          I have read the <p>agreement</p>
-        </Checkbox>
-      </Form.Item>
-      <Form.Item {...tailFormItemLayout}>
-        <Button type="primary" htmlType="submit">
-          Register
-        </Button>
-      </Form.Item>
-    </Form>
+                return Promise.reject(
+                  new Error("The two passwords that you entered do not match!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password  placeholder="Confirm Pasword"/>
+        </Form.Item>
+
+        <Form.Item
+          name="nickname"
+          label="Nickname"
+          tooltip="What do you want others to call you?"
+          rules={[
+            {
+              required: true,
+              message: "Please input your nickname!",
+              whitespace: true,
+            },
+          ]}
+        >
+          <Input  placeholder="NikName"/>
+        </Form.Item>
+
+        <Form.Item
+          label="Captcha"
+          extra="We must make sure that your are a human."
+        >
+          <Row gutter={8}>
+            <Col span={12}>
+              <Form.Item name="captcha" noStyle>
+                <div className="recaptcha">
+                  <ReCAPTCHA sitekey="6LceUGQeAAAAACkNOhVZv0OjckU1BTVA8-WE7Xf0" />
+                </div>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form.Item>
+
+        <Form.Item
+          name="agreement"
+          valuePropName="checked"
+          rules={[
+            {
+              validator: (_, value) =>
+                value
+                  ? Promise.resolve()
+                  : Promise.reject(new Error("Should accept the Tearms and Conditions")),
+            },
+          ]}
+          {...tailFormItemLayout}
+        >
+          <Checkbox>
+            I have read the <p>Tearms and Conditions</p>
+          </Checkbox>
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">
+            Register
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 }
