d508537bad302baf57a878e1efbb3f4787cbfbe3
Pedro <pedroagustinpeyon@gmail.com>
2022-02-12 10:58:41 -0300

    Actions y reducer necesarios sprint 2

--- client/src/FilesStore/Const Types/constActions.js
+++ client/src/FilesStore/Const Types/constActions.js
@@ -2,3 +2,5 @@ export const ALL_HOTELS = "ALL_HOTELS";
 export const DETAIL = "DETAIL";
 export const GOOGLE_LOGIN = "GOOGLE_LOGIN";
 export const GOOGLE_LOGOUT = "GOOGLE_LOGOUT";
+export const ADMIN_STATUS = "ADMIN_STATUS"
+export const CREATE_HOUSE = "CREATE_HOUSE"
