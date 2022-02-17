5926025a7f93c21b6fbc8099317b08cde3a2d20d
Santibio <87582357+Santibio@users.noreply.github.com>
2022-02-17 08:48:41 -0300

    fix - minor fix

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -7,7 +7,7 @@ import ErrorPage from "../pages/ErrorPage";
 import CreateHouse from "../pages/CreatePost";
 import DashboardOwner from "../pages/DashboardOwner";
 import DashboardAdmin from "../pages/DashboardAdmin";
-import DashbiardUser from "../pages/DashboardUser";
+import DashboardUser from "../pages/DashboardUser";
 import RegisterVerify from "../pages/RegisterVerify";
 import ChangePassword from "../pages/ChangePassword";
 import Reservation from "../pages/Reservation";
@@ -28,7 +28,7 @@ function Routes() {
           <Route exact path="/home/:id" component={Detail} />
           <Route exact path="/home/:id/reservation" component={Reservation} />
           <Route exact path="/create" component={CreateHouse} />
-          <Route exact path="/user/:id" component={DashbiardUser} />
+          <Route exact path="/user/:id" component={DashboardUser} />
           <Route exact path="/owner/:id" component={DashboardOwner} />
           <Route exact path="/admin/:id" component={DashboardAdmin} />
           <Route exact path="/register" component={RegisterVerify} />
