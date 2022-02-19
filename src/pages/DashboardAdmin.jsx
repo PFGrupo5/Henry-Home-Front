9a5a994db1f1182856441fa225088c92450878df
TomasGieco <tomas.gieco@hotmail.com>
2022-02-19 17:31:39 -0300

    vercel fix

--- src/pages/DashboardAdmin.jsx
+++ src/pages/DashboardAdmin.jsx
@@ -127,7 +127,7 @@ export default function DashboardAdmin() {
                                     })}
                                 </div>
                                 :
-                                <Loading style="loading" />
+                                <Loading />
                             }
                         </div>
                     ) :
